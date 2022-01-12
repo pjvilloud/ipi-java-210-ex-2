612cda8542f6be77c1dfa52f246d4e7703fe864d
YIHSIU18 <yihsiu.lin@ipilyon.net>
2022-01-12 11:38:54 +0100

    resour interminé

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
-
+/*Exercice 1*/
 public class Main {
+    public static final short MAX_PTS_VIE = 100;
+    public static final short PTS_BOUCLIER = 25;
+    public static final short MAX_ATTAQUE_ENNEMI = 5;
+    public static final short MAX_VIE_ENNEMI = 30;
+    public static final short MAX_ATTAQUE_JOUEUR = 5;
+    public static final short REGENARATION_BOUCLIER_PAR_TOUR = 10;
 
-    /**
-     *
-     * @param args
-     */
+
 }
