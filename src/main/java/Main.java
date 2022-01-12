655317ecb84478e902adc099d70e9e7d19b9a945
YIHSIU18 <yihsiu.lin@ipilyon.net>
2022-01-12 14:14:11 +0100

    resour interminé

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-/*Exercice 1*/
 public class Main {
     public static final short MAX_PTS_VIE = 100;
     public static final short PTS_BOUCLIER = 25;
@@ -8,5 +7,31 @@ public class Main {
     public static final short MAX_ATTAQUE_JOUEUR = 5;
     public static final short REGENARATION_BOUCLIER_PAR_TOUR = 10;
 
+    public static String nomPersonnage;
+    public static short ptsDeVie;
+    public static short ptsBouclier;
+    public static short nbEnnemisTues;
+    public static boolean bouclierActif = true;
 
+    public static void main(String[] args)
+    {
+        initPersonnage();
+    }
+
+    public static void initPersonnage()
+    {   //Afficher le messafe de saisi
+        //Afficher("Saisir le nom de votre personnage")
+        System.out.println("Saisir le nom de votre personnage");
+        //Lire la saisie utilisateur dans ma variable nomPersonnage
+        //Lire(nomPersonnage);
+        Scanner scanner = new Scanner(System.in);
+        nomPersonnage = scanner.nextLine();
+        //Afficher le message c'est parti !
+        System.out.println("OK " + Util.color(nomPersonnage, Color.GREEN) + " ! C'est parti !");
+        //Affecter la variable ptdDeVie
+        ptsDeVie = MAX_PTS_VIE;
+        //Affecter la variable ptsBouclier
+        ptsBouclier = PTS_BOUCLIER;
+        scanner.close();
+    }
 }
